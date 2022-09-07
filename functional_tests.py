0516306f3d1d9145c95c480f7da1e3c509faba05
donghoony <aru0504@naver.com>
2022-09-07 15:49:50 +0900

    First unit test and url mapping

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,4 @@
 import unittest
-
-import selenium.common.exceptions
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,11 @@ from django.test import TestCase
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+
+from django.urls import resolve
+from lists.views import home_page
+
+class PageTest(TestCase):
+    def test_root_pageview(self):
+        root = resolve('/')
+        self.assertEqual(root.func, home_page)
